96e5d6784599079df4f9486f50e2b2f86c21fe4c
Jake Betz <necropyre@outlook.com>
2017-10-30 08:56:15 -0700

    Fixed the bad referencing in LoginLib library and removed unused libraries

--- LoginLib/login.cs
+++ LoginLib/login.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
-using AppGlobals;
 using System.Security.Cryptography;
+using AppGlobals;
 using PasswordHash;
 
 namespace LoginLib
--- LoginSQL/frmLogin.cs
+++ LoginSQL/frmLogin.cs
@@ -1,17 +1,11 @@
-﻿using AppGlobals;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Data;
-using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using LoginLib;
 using PasswordHash;
+using AppGlobals;
 
 namespace LoginSQL
 {
