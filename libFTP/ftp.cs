994931c855789193f0620778480203de35c6e4eb
Jake Betz <necropyre@outlook.com>
2018-09-16 16:31:11 -0700

    Added library to project for ftp connection handling

---
+++ libFTP/ftp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libFTP
+{
+    public class ftp
+    {
+        //todo:  ftp connection handling
+    }
+}
